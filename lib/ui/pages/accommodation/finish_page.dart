1c7ef44c989cc38b4d57e4b82c3faf980786a08e
Severo Cruz <severo_86@hotmail.com>
2025-02-23 19:53:43 -0400

    Pagina final provisional.....

--- lib/ui/pages/accommodation/finish_page.dart
+++ lib/ui/pages/accommodation/finish_page.dart
@@ -21,14 +21,52 @@ class _FinishPageState extends State<FinishPage> {
   Widget build(BuildContext context) {
     _accommodationId = GoRouterState.of(context).extra! as int;
     return Scaffold(body: SafeArea(
-        child: Center(child: AppButton(
-          buttontext: 'Publicar',
-          onclick: (){
-            // context.go('/home');
-            context.pushReplacement('/menu-anfitrion');
-          },
-          context: context
-        ))
+        child: Center(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height*0.5,
+            child: Column(
+              children: [
+                 SizedBox(
+                    height: MediaQuery.of(context).size.height *
+                        0.02), //upar thi jagiya mukeli che
+                // ignore: sized_box_for_whitespace
+            
+                Padding(
+                  padding: const EdgeInsets.symmetric(),
+                  child: Text(
+                    '¡Felicidades!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "Gilroy Bold",
+                        color: Theme.of(context).colorScheme.tertiary), //heding Text
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                Padding(
+                  padding: const EdgeInsets.symmetric(),
+                  child: Text(
+                     'Tu anuncio se creó con éxito',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "Gilroy Bold",
+                        color: Theme.of(context).colorScheme.primary ), //heding Text
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                Center(child: AppButton(
+                  buttontext: 'Pagina Principal',
+                  onclick: (){
+                    // context.go('/home');
+                    context.pushReplacement('/menu-anfitrion');
+                  },
+                  context: context
+                )),
+              ],
+            ),
+          ),
+        )
         ),
       );
   }
